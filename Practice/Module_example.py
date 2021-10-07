50b6690a9802a108b33bc539915350643afd862a
Kristina Engle <kristina1727@gmail.com>
2021-10-07 18:31:02 -0500

    Module 3.4.5 Save

---
+++ Practice/Module_example.py
@@ -0,0 +1,7 @@
+from  youTube_utils import find_max
+
+numbers = [10,3,6,2]
+maximum = find_max(numbers)
+
+print (max(numbers))
+
---
+++ Practice/test_runs.py
@@ -0,0 +1,8 @@
+# Import the datetime class from the datetime module.
+import datetime as dt
+
+#use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+
+# Print the prestent time.
+print("the time right now ", now)
