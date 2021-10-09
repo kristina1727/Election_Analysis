cbb175c069c3c46d66c0941c45436c5a095f70f7
Kristina Engle <kristina1727@gmail.com>
2021-10-09 15:20:26 -0500

    3.4.5 Save Update

--- Practice/Module_example.py
+++
@@ -1,7 +0,0 @@
-from  youTube_utils import find_max
-
-numbers = [10,3,6,2]
-maximum = find_max(numbers)
-
-print (max(numbers))
-
--- PyPoll.py
+++ PyPoll.py
@@ -9,14 +9,20 @@ file_to_load = os.path.join("Resources","election_results.csv")
 file_to_save = os.path.join("Analysis","election_analysis.txt")
 
 # Open the election results and read the file.
-with open(file_to_load) as election_data:
+with open(file_to_load,'r') as election_data:
 
 # To do: read and analyze the data here
     file_reader = csv.reader(election_data)
-
+    
     # Read and print the header row
     headers = next(file_reader)
     print(headers)
+    
+    #print each row in the CSV file
+    #for row in file_reader:
+        #print(row)
+
+
 
 
 # 1. The total number of votes cast
